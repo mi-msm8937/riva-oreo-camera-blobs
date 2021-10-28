dff3ad10a78d61f8cee49b5bbcc3b222d7e5e267
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-28 08:40:37 +0800

    riva: camera: Further fixes for oreo camera blobs

--- camera/QCamera2/stack/common/cam_types.h
+++ camera/QCamera2/stack/common/cam_types.h
@@ -1529,6 +1529,7 @@ typedef struct {
     int32_t est_snap_iso_value;
     uint32_t est_snap_luma;
     uint32_t est_snap_target;
+    uint32_t xiaomi_reserved2;
 } cam_3a_params_t;
 
 typedef struct {
@@ -1911,6 +1912,7 @@ typedef enum {
     CAM_INTF_PARM_ROTATION,
     CAM_INTF_PARM_SCALE,
     CAM_INTF_PARM_VT, /* indicating if it's a Video Call Apllication */
+    XM_01,
     CAM_INTF_META_CROP_DATA,
     CAM_INTF_META_PREP_SNAPSHOT_DONE, /* 60 */
     CAM_INTF_META_GOOD_FRAME_IDX_RANGE,
